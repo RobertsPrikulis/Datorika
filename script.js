d796721ef142e61965bdc90763969b7bd572732a
robissw <145644582+RobertsPrikulis@users.noreply.github.com>
2024-02-29 09:02:53 +0200

    8yg

---
+++ script.js
@@ -0,0 +1,83 @@
+document.getElementById("aiButton").addEventListener("click", function() {
+    document.getElementById("informacijaTeksts").innerHTML = `
+        <h3>1. Azartspēles</h3>
+        <p>
+        <b>Kas tās ir?:</b>
+
+        Azartspēles ir spēles, kurās dalībnieki veic likmes uz rezultātu, kurš ir atkarīgs no nejaušības vai gadījuma. Šīs spēles var ietvert dažādas aktivitātes, piemēram, kazino spēles (piemēram, rulete, blekdžeks, spēļu automāti), kā arī derību veikšanu uz sporta rezultātiem, zirgu sacensībām vai citiem notikumiem. Azartspēles bieži ir saistītas ar risku un var piedāvāt iespēju uzvarēt naudu vai citus priekšmetus.
+        
+        <br><br><b>Azartspēles ,</b> sākās 3000 gadā BCE, Paleolīta periodā. Tajos laikos bija daudz dažādu azartspēļu veidi ,piemēram, senie ķīnieši spēlēja spēles ar kauliem un kartēm, grieķi veica likmes uz sporta sacensībām, un romieši spēlēja dažādas likmju spēles. Tāpat ir arī pierādījumi, ka azartspēles bija populāras senajās Mesopotāmijas un egiptiešu civilizācijās.
+        </p>
+        <p>
+        Tomēr modernās azartspēles, kādas mēs tās saprotam šodien, sāka attīstīties vēlākajos laikos, piemēram, kazino spēles kļuva populāras Eiropā 17. un 18. gadsimtā. Tagad ir populāras šādas spēles: Blekdžeks, pokers, spēļu aparāti, sporta bettings, coinflipi un vel daudz citas spēles kur var izcelt naudu.
+        <p>
+
+
+        <img src="pirmabilde.jpg" width="400" height="300">
+        </p>
+
+       
+
+
+    `;
+});
+
+document.getElementById("blockchainButton").addEventListener("click", function() {
+    document.getElementById("informacijaTeksts").innerHTML = `
+        <h3>2. Blockchain Tehnoloģijas</h3>
+        <p>
+            Blockchain ir decentralizēta, pārbaudāma un neizdzēšama digitālā tehnoloģija, kas ļauj uzglabāt datus bloku ķēdē.<br> Tas ir populārs kriptovalūtu kā Bitcoin pamats, bet tam ir arī daudzas citas pielietošanas jomas, piemēram, tiešsaistes identitāte, neizdzēšamu datu uzglabāšana un vairāk.
+        </p>
+        <p>
+            Blockchain tehnoloģija darbojas, izmantojot decentralizētu datu bāzi, kur datus glabā daudzi datoru tīkla dalībnieki. Katrs jaunais dati tiek pievienots blokam, kas ir piesaistīts iepriekšējam blokam, veidojot ķēdi.
+        </p>
+        <p>
+            Lai pilnībā saprastu Blockchain tehnoloģiju, ir svarīgi izprast tās darbības principus un īpašības. Galvenās īpašības ietver decentralizāciju, neizdzēšamību, kriptogrāfiju un konsensa mehānismu. Decentralizācija nozīmē, ka dati tiek glabāti un pārvaldīti bez centrālās autoritātes, kas nodrošina lielāku drošību un uzticamību. Neizdzēšamība nozīmē, ka reģistrētie dati nevar tikt mainīti vai izdzēsti, nodrošinot nepārtrauktu datu integritāti. Kriptogrāfija nodrošina datu drošību, izmantojot šifrēšanas metodes, kas aizsargā informāciju no neautorizētas piekļuves. Konsensa mehānisms nodrošina vienprātību par jauniem datu ierakstiem un transakcijām visos tīkla dalībniekos.
+        </p>
+    `;
+});
+
+document.getElementById("iotButton").addEventListener("click", function() {
+    document.getElementById("informacijaTeksts").innerHTML = `
+        <h3>3. Internet of Things (IoT)</h3>
+        <p>
+            Internet of Things (IoT) ir tīkls no savstarpēji saistītiem fiziskiem objektiem, kas ir aprīkoti ar sensoriem, programmatūru un citām tehnoloģijām,<br>kas ļauj tiem savstarpēji komunicēt un mainīt datus bez cilvēku iesaistes.<br> IoT tiek izmantots mājsaimniecībās, rūpniecībā, veselības aprūpē un citās jomās.
+        </p>
+        <p>
+            IoT ietver plašu spektru ierīču, kas spēj savstarpēji sazināties un darboties, lai uzlabotu efektivitāti un komfortu. Piemēri ietver viedās mājas sistēmas, rūpniecības sensorus un medicīniskās ierīces.
+        </p>
+        <p>
+            Lai pilnībā saprastu Internet of Things (IoT), ir svarīgi izprast tās darbības principus un pielietojumus. IoT ietver sensoru un saiknes tīklu izmantošanu, lai savienotu un automatizētu ierīces un sistēmas. Šīs sistēmas spēj uzkrāt un apstrādāt lielu daudzumu dati, kas var tikt izmantoti, lai pieņemtu lēmumus, uzlabotu efektivitāti un uzlabotu lietotāju pieredzi. IoT tiek izmantots dažādās nozarēs, piemēram, rūpniecībā, transportācijā, enerģētikā un veselības aprūpē, lai uzlabotu ražošanas procesus, pārvaldītu infrastruktūru un nodrošinātu pacientu aprūpi.
+        </p>
+    `;
+});
+
+document.getElementById("arvrButton").addEventListener("click", function() {
+    document.getElementById("informacijaTeksts").innerHTML = `
+        <h3>4. Augmented Reality (AR) un Virtual Reality (VR)</h3>
+        <p>
+            Paplašinātā realitāte (AR) un virtuālā realitāte (VR) ir tehnoloģijas, kas ļauj lietotājiem mijiedarboties ar virtuālu vai paplašinātu vidi.<br> AR papildina reālo pasauli ar virtuāliem objektiem, kamēr VR piedāvā pilnīgi iedomātu vidi, ko lietotājs var izpētīt un ietekmēt.
+        </p>
+        <p>
+            AR un VR tiek izmantotas plašā spektrā nozarēs, ieskaitot izglītību, izklaides un rūpniecību. Piemēri ietver viedās brilles, kuras papildina reālo pasauli ar virtuāliem objektiem, un VR galvasseglus, kas ļauj lietotājiem ieiet pilnīgi virtuālās vides.
+        </p>
+        <p>
+            Lai saprastu Augmented Reality (AR) un Virtual Reality (VR) nozīmi un pielietojumus, ir svarīgi izprast to tehnoloģisko bāzi un iespējas. AR ļauj paplašināt reālo pasauli ar virtuālām informācijas projekcijām, kurās lietotāji var mijiedarboties un sajust paplašinātās realitātes elementus. VR nodrošina pilnīgi jaunu virtuālo vidi, kurā lietotāji var ieiet un izpētīt virtuālus pasaulus. Šīs tehnoloģijas tiek izmantotas izglītībā, lai uzlabotu mācīšanos un pieredzi, izklaides nozarē, lai radītu interaktīvas spēles un pieredzes, un rūpniecībā, lai simulētu treniņus un izmēģinājumus.
+        </p>
+    `;
+});
+
+document.getElementById("cloudButton").addEventListener("click", function() {
+    document.getElementById("informacijaTeksts").innerHTML = `
+        <h3>5. Cloud Computing</h3>
+        <p>
+            Mākoņskaitļošana (Cloud Computing) ir modelis, kas ļauj piekļūt datora resursiem, piemēram, datu krātuvei un skaitļošanas jaudai, pār tīklu (parasti internetu), pēc pieprasījuma.<br> Tas nodrošina elastīgumu, skalējamību un resursu kopīgošanu dažādās organizācijās un nozarēs.
+        </p>
+        <p>
+            Cloud Computing ir kļuvis par plaši izmantotu risinājumu dažādās nozarēs, tostarp uzņēmējdarbībā, izglītībā un valsts iestādēs. Tas ļauj organizācijām piekļūt datora resursiem bez nepieciešamības iegādāties un uzturēt savu infrastruktūru.
+        </p>
+        <p>
+            Lai pilnībā saprastu Cloud Computing nozīmi un priekšrocības, ir svarīgi izprast tās darbības principus un pielietojumus. Cloud Computing nodrošina elastīgu un skalējamu piekļuvi datora resursiem, kas ļauj organizācijām pielāgoties mainīgajiem vajadzībām un pieprasījumiem. Tas arī nodrošina resursu kopīgošanu un sadalīšanu, kas samazina izmaksas un palielina efektivitāti. Cloud Computing piedāvā dažādas pakalpojumu modeļus, tostarp infrastruktūras kā pakalpojumu (IaaS), platformas kā pakalpojumu (PaaS) un programmatūras kā pakalpojumu (SaaS), kas atbilst dažādām organizāciju vajadzībām un prasībām.
+        </p>
+    `;
+});
