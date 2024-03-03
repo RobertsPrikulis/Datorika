0b81cf2d37e13783db48c8382219257c39307282
robissw <145644582+RobertsPrikulis@users.noreply.github.com>
2024-03-03 18:45:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById("aiButton").addEventListener("click", function() {
         <p>
         <b>Kas tās ir?:</b>
 
-        Azartspēles ir spēles, kurās dalībnieki veic likmes uz rezultātu, kurš ir atkarīgs no nejaušības vai gadījuma. Šīs spēles var ietvert dažādas aktivitātes, piemēram, kazino spēles (piemēram, rulete, blekdžeks, spēļu automāti), kā arī derību veikšanu uz sporta rezultātiem, zirgu sacensībām vai citiem notikumiem. Azartspēles bieži ir saistītas ar risku un var piedāvāt iespēju uzvarēt naudu vai citus priekšmetus.
+        Azartspēles ir ir ir spēles, kurās dalībnieki veic likmes uz rezultātu, kurš ir atkarīgs no nejaušības vai gadījuma. Šīs spēles var ietvert dažādas aktivitātes, piemēram, kazino spēles (piemēram, rulete, blekdžeks, spēļu automāti), kā arī derību veikšanu uz sporta rezultātiem, zirgu sacensībām vai citiem notikumiem. Azartspēles bieži ir saistītas ar risku un var piedāvāt iespēju uzvarēt naudu vai citus priekšmetus.
         
         <br><br><b>Azartspēles ,</b> sākās 3000 gadā BCE, Paleolīta periodā. Tajos laikos bija daudz dažādu azartspēļu veidi ,piemēram, senie ķīnieši spēlēja spēles ar kauliem un kartēm, grieķi veica likmes uz sporta sacensībām, un romieši spēlēja dažādas likmju spēles. Tāpat ir arī pierādījumi, ka azartspēles bija populāras senajās Mesopotāmijas un egiptiešu civilizācijās.
         </p>
@@ -24,60 +24,50 @@ document.getElementById("aiButton").addEventListener("click", function() {
 
 document.getElementById("blockchainButton").addEventListener("click", function() {
     document.getElementById("informacijaTeksts").innerHTML = `
-        <h3>2. Blockchain Tehnoloģijas</h3>
+        <h3>2. Azartspēles kuras es iesaku pamēgināt.</h3>
         <p>
-            Blockchain ir decentralizēta, pārbaudāma un neizdzēšama digitālā tehnoloģija, kas ļauj uzglabāt datus bloku ķēdē.<br> Tas ir populārs kriptovalūtu kā Bitcoin pamats, bet tam ir arī daudzas citas pielietošanas jomas, piemēram, tiešsaistes identitāte, neizdzēšamu datu uzglabāšana un vairāk.
+        Pirmā spēle kuru es ieteiktu būtu Blakdžeks (blackjack), šī spēle ir laba priekš iesācējiem, jo to ir viegli iemācīties spēlet un ir labākāš iespējas uzvarēt naudu.
         </p>
         <p>
-            Blockchain tehnoloģija darbojas, izmantojot decentralizētu datu bāzi, kur datus glabā daudzi datoru tīkla dalībnieki. Katrs jaunais dati tiek pievienots blokam, kas ir piesaistīts iepriekšējam blokam, veidojot ķēdi.
+        Tālāk es ieteiktu ruleti, šī spēle ir vienkārša un maksā arī labi. Šeit ir jaliek nauda uz saviem mīļākajiem skaitļiem, 
+        dīleris griež ratu un ja grieztā bumbiņa uzkrīt uz tava skaitļa tu uzvari. 
+        Šajā spēlē arī var likt uz sarkano un melno, ja tā dara tad iespējas vinnēt ir 50-50.
         </p>
         <p>
-            Lai pilnībā saprastu Blockchain tehnoloģiju, ir svarīgi izprast tās darbības principus un īpašības. Galvenās īpašības ietver decentralizāciju, neizdzēšamību, kriptogrāfiju un konsensa mehānismu. Decentralizācija nozīmē, ka dati tiek glabāti un pārvaldīti bez centrālās autoritātes, kas nodrošina lielāku drošību un uzticamību. Neizdzēšamība nozīmē, ka reģistrētie dati nevar tikt mainīti vai izdzēsti, nodrošinot nepārtrauktu datu integritāti. Kriptogrāfija nodrošina datu drošību, izmantojot šifrēšanas metodes, kas aizsargā informāciju no neautorizētas piekļuves. Konsensa mehānisms nodrošina vienprātību par jauniem datu ierakstiem un transakcijām visos tīkla dalībniekos.
+        <img src="black.jpg" width="400" height="300">   <img src="rulete.jpg" width="400" height="300">
         </p>
     `;
 });
 
 document.getElementById("iotButton").addEventListener("click", function() {
     document.getElementById("informacijaTeksts").innerHTML = `
-        <h3>3. Internet of Things (IoT)</h3>
+        <h3>3. Provably fair </h3>
         <p>
-            Internet of Things (IoT) ir tīkls no savstarpēji saistītiem fiziskiem objektiem, kas ir aprīkoti ar sensoriem, programmatūru un citām tehnoloģijām,<br>kas ļauj tiem savstarpēji komunicēt un mainīt datus bez cilvēku iesaistes.<br> IoT tiek izmantots mājsaimniecībās, rūpniecībā, veselības aprūpē un citās jomās.
+        Pierādāms godīgs (provably fair) nozīmē, ka spēles ir veidotas tā, lai tās būtu caurspīdīgas un godīgas.
+Pierādāmi godīgas spēles tiek uzskatītas par caurspīdīgākām un godīgākām nekā tradicionālās tiešsaistes kazino spēles,
+ jo tās ļauj spēlētājiem redzēt, kā veidojas katras kārtas iznākums.
         </p>
         <p>
-            IoT ietver plašu spektru ierīču, kas spēj savstarpēji sazināties un darboties, lai uzlabotu efektivitāti un komfortu. Piemēri ietver viedās mājas sistēmas, rūpniecības sensorus un medicīniskās ierīces.
         </p>
         <p>
-            Lai pilnībā saprastu Internet of Things (IoT), ir svarīgi izprast tās darbības principus un pielietojumus. IoT ietver sensoru un saiknes tīklu izmantošanu, lai savienotu un automatizētu ierīces un sistēmas. Šīs sistēmas spēj uzkrāt un apstrādāt lielu daudzumu dati, kas var tikt izmantoti, lai pieņemtu lēmumus, uzlabotu efektivitāti un uzlabotu lietotāju pieredzi. IoT tiek izmantots dažādās nozarēs, piemēram, rūpniecībā, transportācijā, enerģētikā un veselības aprūpē, lai uzlabotu ražošanas procesus, pārvaldītu infrastruktūru un nodrošinātu pacientu aprūpi.
+        <img src="prov.jpg" width="300" height="300">
         </p>
     `;
 });
 
 document.getElementById("arvrButton").addEventListener("click", function() {
     document.getElementById("informacijaTeksts").innerHTML = `
-        <h3>4. Augmented Reality (AR) un Virtual Reality (VR)</h3>
+        <h3>4. Azartspēļu atkarība</h3>
         <p>
-            Paplašinātā realitāte (AR) un virtuālā realitāte (VR) ir tehnoloģijas, kas ļauj lietotājiem mijiedarboties ar virtuālu vai paplašinātu vidi.<br> AR papildina reālo pasauli ar virtuāliem objektiem, kamēr VR piedāvā pilnīgi iedomātu vidi, ko lietotājs var izpētīt un ietekmēt.
+        Azartspēļu atkarība, kas pazīstama arī kā kompulsīvā azartspēle vai patoloģiska azartspēle, ir nopietns traucējums, ko raksturo nekontrolējama vēlme spēlēt azartspēles,
+ neskatoties uz negatīvām sekām. Tāpat kā citi atkarības veidi, tā var izraisīt ievērojamas finansiālas, sociālas un psiholoģiskas problēmas.
         </p>
         <p>
-            AR un VR tiek izmantotas plašā spektrā nozarēs, ieskaitot izglītību, izklaides un rūpniecību. Piemēri ietver viedās brilles, kuras papildina reālo pasauli ar virtuāliem objektiem, un VR galvasseglus, kas ļauj lietotājiem ieiet pilnīgi virtuālās vides.
+        Ja jum ir azartspēļu atkarība ir svarīgi meklēt palīdzību no kvalificētiem speciālistiem. Ir pieejami daudzi resursi un atbalsta tīkli, lai palīdzētu personām pārvarēt šo sarežģīto traucējumu.
         </p>
         <p>
-            Lai saprastu Augmented Reality (AR) un Virtual Reality (VR) nozīmi un pielietojumus, ir svarīgi izprast to tehnoloģisko bāzi un iespējas. AR ļauj paplašināt reālo pasauli ar virtuālām informācijas projekcijām, kurās lietotāji var mijiedarboties un sajust paplašinātās realitātes elementus. VR nodrošina pilnīgi jaunu virtuālo vidi, kurā lietotāji var ieiet un izpētīt virtuālus pasaulus. Šīs tehnoloģijas tiek izmantotas izglītībā, lai uzlabotu mācīšanos un pieredzi, izklaides nozarē, lai radītu interaktīvas spēles un pieredzes, un rūpniecībā, lai simulētu treniņus un izmēģinājumus.
+        <img src="last.jpg" width="300" height="300">
         </p>
     `;
 });
 
-document.getElementById("cloudButton").addEventListener("click", function() {
-    document.getElementById("informacijaTeksts").innerHTML = `
-        <h3>5. Cloud Computing</h3>
-        <p>
-            Mākoņskaitļošana (Cloud Computing) ir modelis, kas ļauj piekļūt datora resursiem, piemēram, datu krātuvei un skaitļošanas jaudai, pār tīklu (parasti internetu), pēc pieprasījuma.<br> Tas nodrošina elastīgumu, skalējamību un resursu kopīgošanu dažādās organizācijās un nozarēs.
-        </p>
-        <p>
-            Cloud Computing ir kļuvis par plaši izmantotu risinājumu dažādās nozarēs, tostarp uzņēmējdarbībā, izglītībā un valsts iestādēs. Tas ļauj organizācijām piekļūt datora resursiem bez nepieciešamības iegādāties un uzturēt savu infrastruktūru.
-        </p>
-        <p>
-            Lai pilnībā saprastu Cloud Computing nozīmi un priekšrocības, ir svarīgi izprast tās darbības principus un pielietojumus. Cloud Computing nodrošina elastīgu un skalējamu piekļuvi datora resursiem, kas ļauj organizācijām pielāgoties mainīgajiem vajadzībām un pieprasījumiem. Tas arī nodrošina resursu kopīgošanu un sadalīšanu, kas samazina izmaksas un palielina efektivitāti. Cloud Computing piedāvā dažādas pakalpojumu modeļus, tostarp infrastruktūras kā pakalpojumu (IaaS), platformas kā pakalpojumu (PaaS) un programmatūras kā pakalpojumu (SaaS), kas atbilst dažādām organizāciju vajadzībām un prasībām.
-        </p>
-    `;
-});
